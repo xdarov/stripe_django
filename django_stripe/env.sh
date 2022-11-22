5fcdb18a94d6cd9150f1983d383579df20054d00
Danis Khaydarov <89683379+xdarov@users.noreply.github.com>
2022-11-22 19:02:22 +0300

    env

---
+++ django_stripe/env.sh
@@ -0,0 +1,11 @@
+export 'DB_ENGINE=' \
+    'DB_NAME=' \
+    'DB_USER=' \
+    'DB_PASSWORD=' \
+    'DB_HOST=' \
+    'DB_PORT=' \
+    'STRYPE_API_KEY=' \
+    'SECRET_KEY=' \
+    'DOMEN='
+
+python manage.py runserver
